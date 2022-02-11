968987ef9b73d055f364997cb63cd924edbacf46
Emma McCracken <emma.mccracken14@gmail.com>
2022-02-11 17:37:09 +0000

    begin writing functions to map state to nodes and update typescript

---
+++ src/utils/makeChoiceElements.ts
@@ -0,0 +1,37 @@
+import { ChoiceI, StateI } from "./Interfaces";
+
+export {};
+
+function makeChoicesElements(state: StateI): any {
+  // let element = {
+  //     id: "1",
+  //     data: {
+  //       label: (
+  //         <>
+  //           <h3>Taste: </h3>
+  //           <p>Weighting: {state.attributes[0].weighting}</p>
+  //           <Box className="nodrag" sx={{ width: 100, margin: "0 auto" }}>
+  //             <Slider
+  //               defaultValue={50}
+  //               onChange={(e) => {
+  //                 const element = e.target as HTMLInputElement;
+  //                 dispatch({
+  //                   type: "update_attribute_weighting",
+  //                   choiceId: 0,
+  //                   attributeId: 0,
+  //                   weighting: parseFloat(element.value),
+  //                 });
+  //               }}
+  //               min={0}
+  //               max={1}
+  //               step={0.1}
+  //             />
+  //           </Box>
+  //         </>
+  //       ),
+  //     },
+  //     position: { x: 250, y: 20 },
+  //   }
+
+  return {};
+}
