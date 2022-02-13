06ddef55bbaf92e3b0f5362fa60818e2150f7557
Emma McCracken <emma.mccracken14@gmail.com>
2022-02-13 09:25:58 +0000

    update description of test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
+test("renders title in header", () => {
   render(<App />);
   const linkElement = screen.getByText(/Decision Making Helper/i);
   expect(linkElement).toBeInTheDocument();
